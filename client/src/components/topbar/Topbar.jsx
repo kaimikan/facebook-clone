75d53ced15b3d56d593d11334b409ec81cb82c5d
kaimikan <oihic@protonmail.com>
2022-05-01 14:15:11 +0200

    Separate Server & Add Client Base

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./topbar.css";
+// doing separate imports to not overload things by fetching the entire "@mui/icons-material" library
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">FacBuk</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input type="text" placeholder="Search..." className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/3.jpeg" alt="" className="topbarImage" />
+      </div>
+    </div>
+  );
+}
